b8e96ab992587329d98db3d01c8f0f98a81e199c
Nick Graves <83090027+nick-graves@users.noreply.github.com>
2023-05-31 11:47:11 -0700

    Add files via upload

--- cs_340_rundle/app.js
+++ cs_340_rundle/app.js
@@ -1,39 +1,42 @@
-// var db = require('./db_connector')
-
-
-
-
-// const express = require('express');
-// const app = express();
-// app.use(express.static('public'));
-// const port = 3000;
-// app.use(express.json());
-
-// var db = require('./db-connector');
 var db = require('./db_connector')
 
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-const PORT  = 3000;                 // Set a port number at the top so it's easy to change in the future
+const PORT  = 4000;                 // Set a port number at the top so it's easy to change in the future
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Handlebars
 const { engine } = require('express-handlebars');
+// const { runInNewContext } = require('vm')
 var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', exphbs({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.engine('.hbs', engine({extname: ".hbs"}));
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters an *.hbs file.
 
 // Make public directory (CSS, etc,.) available to client (user's browser)
 app.use(express.static('public'));
 
 
-app.get('/', function(req, res){
 
-    res.render('main.hbs');
+
+
+app.get('/', function(req, res)
+{
+
+    res.render('homePage', {});
 });
 
+app.get('/hikes', function(req, res)
+{
+    res.render('hikes', {});
+});
+
+app.get('/edit_hikes', function(req, res)
+{
+
+    res.render('edit_hikes', {});
+});
 
 
 
